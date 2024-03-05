9b36e7e90d21a8aaf2d1596e06a682a5586d5234
stepband <vladshvik@gmail.com>
2024-03-05 15:20:00 +0200

    Implemented models for cinema manager application:
    - Added User model for user management.
    - Added Genre model to categorize movies.
    - Added Movie model to store movie details such as title, genre, release date, and duration.
    - Added TheaterRoom model to represent theater rooms where screenings take place.
    - Added Seat model to represent individual seats within theater rooms, including row number, column number, and availability status.
    - Updated Ticket model to include a reference to the Seat model, allowing tickets to be associated with specific seats in screenings.
    - Updated Screening model to include a reference to the TheaterRoom model, linking each screening to a specific theater room.
    - Each model includes appropriate string representations for better readability.

--- ticketing/models.py
+++ ticketing/models.py
@@ -1,3 +1,54 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import PROTECT
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Genre: {self.name}"
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    genre = models.ManyToManyField(Genre, related_name="movie", blank=True)
+    release_date = models.DateField(null=True)
+    duration = models.DurationField(null=True)
+
+
+class TheaterRoom(models.Model):
+    room_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Room Name: {self.room_name}"
+
+
+class Seat(models.Model):
+    theater_room = models.ForeignKey('TheaterRoom', on_delete=models.CASCADE)
+    row_number = models.IntegerField()
+    column_number = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"Seat {self.row_number}-{self.column_number} ({'Available' if self.is_available else 'Booked'})"
+
+
+class Screening(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    theater_room = models.ForeignKey(TheaterRoom, on_delete=models)
+
+    def __str__(self):
+        return f"{self.movie.title} - {self.date} {self.time}"
+
+
+class Ticket(models.Model):
+    user = models.ForeignKey(User, on_delete=PROTECT)
+    screening = models.ForeignKey(Screening, on_delete=PROTECT)
+    seat = models.ForeignKey(Seat, on_delete=models.CASCADE)

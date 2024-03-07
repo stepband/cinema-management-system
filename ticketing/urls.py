2eb3cbe0d2d47f595f4fa7d9aeb5e4165f7c4f34
stepband <vladshvik@gmail.com>
2024-03-07 10:13:50 +0200

    Added template and fixtures folders, configured main URLs to redirect to ticketing URLs
    - Created template folder with index.html and base.html templates
    - Added fixtures folder with genres.json and movies.json
    - Configured main project URLs to redirect to ticketing app URLs
    - Added FIXTURE_DIRS setting in settings.py
    - Implemented class-based view for index page

---
+++ ticketing/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import MovieListView
+
+urlpatterns = [
+    path('', MovieListView.as_view(), name='index'),
+]
--- ticketing/views.py
+++ ticketing/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Movie
 
-# Create your views here.
+
+class MovieListView(ListView):
+    model = Movie
+    template_name = 'index.html'
+    context_object_name = 'movies'

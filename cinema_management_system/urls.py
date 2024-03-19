15e63b2053b2b5a545ba6775df92968b276f8fe5
stepband <vladshvik@gmail.com>
2024-03-19 18:00:45 +0200

    - In base.html:   - Added blocks for title and content. - In index.html:   - Added a basic view for the movies table. - Updated urls.py to redirect to ticketing/urls.py.

--- cinema_management_system/urls.py
+++ cinema_management_system/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ticketing', include('ticketing.urls'))
+    path('', include('ticketing.urls')),
 ]
